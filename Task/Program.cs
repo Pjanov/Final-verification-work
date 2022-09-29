be0372c0b54c883e2858e13bdab3e6c47e06b231
Андрей <pjanov84@mail.ru>
2022-09-29 15:37:14 +0300

    Recorded the solution

--- Task/Program.cs
+++ Task/Program.cs
@@ -5,8 +5,30 @@
 Пример:
 ["hello", "2", "world", "123"]->["2", "123"]     */
 
+void PrintArray(string[] array)  // вывод существующего массива
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"\"{array[i]}\"{(i != array.Length - 1 ? ", " : "")}");
+    }
+    Console.Write("]");
+}
 
+void GeneratedAarray(string[] array) // формирует массив из строк, длина которых <=3 символам.
+{
+    Console.Write("->[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write($"\"{array[i]}\"{(i != array.Length - 1 ? ", " : "")}");
+        }
+    }
+    Console.Write("]");
+}
 
+string[] array = { "hello", "2", "world", "123" };
 
-
-
+PrintArray(array);
+GeneratedAarray(array);
